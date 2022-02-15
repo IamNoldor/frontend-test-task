3874608c2f81afe875e298ae6a97b85ad745580f
noldor <noldorvlad@gmail.com>
2022-02-15 02:13:57 +0200

    Frontend

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showNavigation() {
+    let nav_block = document.getElementById("nav__block-sm");
+    nav_block.style.left = "0";
+}
+function closeNavigation() {
+    let nav_block = document.getElementById("nav__block-sm");
+    nav_block.style.left = "-400px";
+}
